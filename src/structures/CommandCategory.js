f437a5ce796a5ec5a2f55a8e348bc87b9aa65b31
jimkalama77 <jimkalama77@gmail.com>
2022-09-01 22:48:32 +0700

    image changes

--- src/structures/CommandCategory.js
+++ src/structures/CommandCategory.js
@@ -1,82 +1,82 @@
 module.exports = {
   ADMIN: {
     name: "Admin",
-    image: "https://icons.iconarchive.com/icons/dakirby309/simply-styled/256/Settings-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FAUTOMOD.png?alt=media&token=19de8270-729f-41ff-a0d7-f6c54a4b764c",
     emoji: "⚙️",
   },
   AUTOMOD: {
     name: "Automod",
-    image: "https://icons.iconarchive.com/icons/dakirby309/simply-styled/256/Settings-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FAUTOMOD.png?alt=media&token=19de8270-729f-41ff-a0d7-f6c54a4b764c",
     emoji: "🤖",
   },
   ANIME: {
     name: "Anime",
-    image: "https://wallpaperaccess.com/full/5680679.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FANIME.png?alt=media&token=65627af8-17c3-428e-a6f2-dc1e091b64a1",
     emoji: "🎨",
   },
   ECONOMY: {
     name: "Economy",
-    image: "https://icons.iconarchive.com/icons/custom-icon-design/pretty-office-11/128/coins-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FECONOMY.png?alt=media&token=36b927ca-1b3c-41c4-afc6-dda6f2b52aef",
     emoji: "🪙",
   },
   FUN: {
     name: "Fun",
-    image: "https://icons.iconarchive.com/icons/flameia/aqua-smiles/128/make-fun-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FSMILE.png?alt=media&token=1c001230-0f2b-4996-ba87-e62e26c9a8ee",
     emoji: "😂",
   },
   GIVEAWAY: {
     name: "Giveaway",
-    image: "https://icons.iconarchive.com/icons/flameia/aqua-smiles/128/make-fun-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FGIVEAWAY.png?alt=media&token=df431b6b-9fac-4dc6-a197-f2ddcb8ddd67",
     emoji: "🎉",
   },
   IMAGE: {
     name: "Image",
-    image: "https://icons.iconarchive.com/icons/dapino/summer-holiday/128/photo-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FIMAGE.png?alt=media&token=a59bfe8b-5d05-40be-9946-513a2a82c2b1",
     emoji: "🖼️",
   },
   INVITE: {
     name: "Invite",
-    image: "https://cdn4.iconfinder.com/data/icons/general-business/150/Invite-512.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FINVITE.png?alt=media&token=3bf3777d-b3c8-42cb-b7b7-d492dbb0a6ad",
     emoji: "📨",
   },
   INFORMATION: {
     name: "Information",
-    image: "https://icons.iconarchive.com/icons/graphicloads/100-flat/128/information-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FINFORMATION.png?alt=media&token=bbaa7982-7112-467c-8e7b-f2069ed8c1e1",
     emoji: "🪧",
   },
   MODERATION: {
     name: "Moderation",
-    image: "https://icons.iconarchive.com/icons/lawyerwordpress/law/128/Gavel-Law-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FHAMMERS.png?alt=media&token=24cb4fe7-27e5-4d45-a170-13f348ac61ec",
     emoji: "🔨",
   },
   MUSIC: {
     name: "Music",
-    image: "https://icons.iconarchive.com/icons/wwalczyszyn/iwindows/256/Music-Library-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FMUSIC.png?alt=media&token=b5372804-6179-411b-a810-91a645cd9be6",
     emoji: "🎵",
   },
   OWNER: {
     name: "Owner",
-    image: "https://www.pinclipart.com/picdir/middle/531-5318253_web-designing-icon-png-clipart.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FOWNER.png?alt=media&token=9a55a1f8-38fe-4c46-9417-541ef5d0b895",
     emoji: "🤴",
   },
   SOCIAL: {
     name: "Social",
-    image: "https://icons.iconarchive.com/icons/dryicons/aesthetica-2/128/community-users-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FSOCIAL.png?alt=media&token=7a4bd325-13f1-4cd4-8711-a2d56fd20dd8",
     emoji: "🫂",
   },
   SUGGESTION: {
     name: "Suggestion",
-    image: "https://cdn-icons-png.flaticon.com/512/1484/1484815.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FSUGGESTION.png?alt=media&token=81b7c71f-84cd-4729-81ae-1c7532133663",
     emoji: "📝",
   },
   TICKET: {
     name: "Ticket",
-    image: "https://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/512/ticket-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FTICKET.png?alt=media&token=cfe7a1e5-1b25-4cf9-9022-1751abf9e721",
     emoji: "🎫",
   },
   UTILITY: {
     name: "Utility",
-    image: "https://icons.iconarchive.com/icons/blackvariant/button-ui-system-folders-alt/128/Utilities-icon.png",
+    image: "https://firebasestorage.googleapis.com/v0/b/boxcube-33f6d.appspot.com/o/pawws%2FUTILITIES.png?alt=media&token=4f6ea209-1fd0-458e-8c17-e676d3482dce",
     emoji: "🛠",
   },
 };
